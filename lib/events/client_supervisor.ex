3344019ea66445f965fa1f0f1962d26b1d2dc1cc
Alejandro Prado <alejandro.prado@grindr.com>
2017-07-18 09:45:07 -0300

    adding client supervisor

---
+++ lib/events/client_supervisor.ex
@@ -0,0 +1,12 @@
+defmodule ClientSupervisor do
+  use Supervisor
+
+  def start_link() do
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    children = [worker((), [])]
+    opts= [strategy: :one_for_one]
+  end
+end
