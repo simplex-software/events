0073ee370cf7479fa63a7de4946b3c2c42f155cb
Guillermo <guillermo.alvarez@grindr.com>
2017-08-17 10:28:42 -0300

    Adding logging.

--- lib/events/users/user_service.ex
+++ lib/events/users/user_service.ex
@@ -1,16 +1,21 @@
 defmodule Users.UserService do
+  defstruct users: [], count: 0
   use GenServer
 
   def start_link() do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, %Users.UserService{})
   end
 
-  def init(pid) do
-    {:ok, pid}
+  def init(state) do
+    {:ok, state}
   end
 
   def new_user(pid, name, email, password) do
     GenServer.call(pid, {:new_user, name, email, password})
+  end
+
+  def login(pid, email, password) do
+    GenServer.call(pid, {:login, email, password})
   end
 
   def get_users(pid) do
@@ -19,20 +24,24 @@ defmodule Users.UserService do
 
   #CALLBACKS
 
-  def handle_call({:new_user, name, email, password}, _from, users) do
-    {:ok, user} = Users.User.new(name, email, password, next_user_id(users))
-    users = [user | users]
-    {:reply, {:ok, user.id}, users}
+  def handle_call({:new_user, name, email, password}, _from, state) do
+    user_id = state.count + 1
+    {:ok, user} = Users.User.new(name, email, password, user_id)
+    state = %{state | users: [user | state.users], count: user_id}
+    {:reply, {:ok, user.id}, state}
   end
 
   def handle_call({:get_users}, _from, users) do
     {:reply, {:ok, users}, {:ok, users}}
   end
 
-  ## PRIVATE FUNCTIONS
-  defp next_user_id(users) do
-    max_id = Enum.map(users, fn(user) -> user.id end) |>
-      Enum.max(fn() -> 0 end)
-    max_id + 1
+  def handle_call({:login, email, password}, _from, state) do
+    user = Enum.find(state.users, fn(element) ->
+      email == element.email && Users.User.verify_password(password, element.hashed_password)
+    end)
+    case user do
+      nil -> {:reply, {:error, "User not found"}, state}
+      user -> {:reply, {:ok, user}, state}
+    end
   end
 end
--- test/events/users/user_service_test.exs
+++ test/events/users/user_service_test.exs
@@ -14,14 +14,27 @@ defmodule Events.Users.UseServiceTest do
   end
 
   test "Get users", context do
-    {:ok, userId} = UserService.new_user(context[:serverPid], "TestUser", "test@simplex.com", "12345")
-    {:ok, userId} = UserService.new_user(context[:serverPid], "TestUser2", "test2@simplex.com", "12345")
+    {:ok, userId1} = UserService.new_user(context[:serverPid], "TestUser", "test@simplex.com", "12345")
+    {:ok, userId2} = UserService.new_user(context[:serverPid], "TestUser2", "test2@simplex.com", "12345")
     {:ok, users}  = UserService.get_users(context[:serverPid])
+    assert Enum.find(users.users, fn(u) -> u.id == userId1 end) != nil
+    assert Enum.find(users.users, fn(u) -> u.id == userId2 end) != nil
+    assert Enum.find(users.users, fn(u) -> u.id == "4444" end) == nil
   end
 
   test "Create users generate incremental IDs", context do
     {:ok, user1} = UserService.new_user(context[:serverPid], "TestUser", "test@simplex.com", "12345")
     {:ok, user2} = UserService.new_user(context[:serverPid], "TestUser2", "test2@simplex.com", "12345")
     assert user2 > user1
+  end
+
+  test "Login doesn't log in unexisting user", context do
+    {:error, "User not found"} = UserService.login(context[:serverPid], "test@simplex.com", "12345")
+  end
+
+  test "Login happy path", context do
+    {:ok, userId} = UserService.new_user(context[:serverPid], "TestUser", "test@simplex.com", "12345")
+    {:ok, loggedUser} = UserService.login(context[:serverPid], "test@simplex.com", "12345")
+    assert loggedUser.id == userId
   end
 end
